02eca9616dd8a29b54890b59d1eddcb73e5089cb
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-08-18 15:21:30 -0500

    Included new components in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+// Components
+import BooksList from './BooksList';
+import BooksForm from './BooksForm';
+
 export default function App() {
   return (
     <div>
+      <BooksForm />
+      <BooksList />
     </div>
   );
 }
