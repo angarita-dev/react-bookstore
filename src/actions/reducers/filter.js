77441694ccb90822ae3e5652ce766b0b2ee56e9b
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-08-19 13:11:03 -0500

    Modified reducer to return a string

--- src/actions/reducers/filter.js
+++ src/actions/reducers/filter.js
@@ -3,7 +3,7 @@ import { CHANGE_FILTER } from '../index';
 const filter = (state = 'All', action) => {
   switch (action.type) {
     case CHANGE_FILTER:
-      return action.category;
+      return action.category.filter;
     default:
       return state;
   }
