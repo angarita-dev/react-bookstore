2f460d6f98ae7bf87633eea68d0fa9da90ba98ea
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-08-19 00:49:20 -0500

    Included action & passes it to Book component

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -2,11 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+// Actions
+import { removeBook } from '../actions/index';
+
 // Components
 import Book from '../components/Book';
 
 const BooksList = props => {
   const { books } = props;
+  const { removeBook } = props;
+
   const booksList = books.map(book => {
     const { id, title, category } = book;
     return (
@@ -15,6 +20,7 @@ const BooksList = props => {
         id={id}
         title={title}
         category={category}
+        removeBook={removeBook}
       />
     );
   });
@@ -26,6 +32,7 @@ const BooksList = props => {
           <th>Book ID</th>
           <th>Title</th>
           <th>Category</th>
+          <th>Remove</th>
         </tr>
       </thead>
       <tbody>
@@ -37,10 +44,11 @@ const BooksList = props => {
 
 BooksList.propTypes = {
   books: PropTypes.array.isRequired,
+  removeBook: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   books: state.books,
 });
 
-export default connect(mapStateToProps)(BooksList);
+export default connect(mapStateToProps, { removeBook })(BooksList);
