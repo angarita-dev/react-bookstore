48a8de1fa8457384002f00e89ad20fb3a5df783f
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-08-19 13:12:40 -0500

    Passed action to filter component

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 // Actions
-import { removeBook } from '../actions/index';
+import { removeBook, changeFilter } from '../actions/index';
 
 // Components
 import CategoryFilter from '../components/CategoryFilter';
@@ -12,9 +12,15 @@ import Book from '../components/Book';
 const BooksList = props => {
   const { books } = props;
   const { removeBook } = props;
+  const { changeFilter } = props;
   const { categoryList } = props;
+  const { currentFilter } = props;
 
-  const booksList = books.map(book => {
+  const filteredBooks = currentFilter === 'All' ? 
+    books :
+    books.filter(book => currentFilter === book.category);
+
+  const booksList = filteredBooks.map(book => {
     const { id, title, category } = book;
     return (
       <Book
@@ -37,7 +43,7 @@ const BooksList = props => {
           <th>Remove</th>
           <th>
             Filter:
-            <CategoryFilter categoryList={categoryList} />
+            <CategoryFilter categoryList={categoryList} changeFilter={changeFilter}/>
           </th>
         </tr>
       </thead>
@@ -52,10 +58,12 @@ BooksList.propTypes = {
   books: PropTypes.array.isRequired,
   removeBook: PropTypes.func.isRequired,
   categoryList: PropTypes.array.isRequired,
+  currentFilter: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = state => ({
   books: state.books,
+  currentFilter: state.filter,
 });
 
-export default connect(mapStateToProps, { removeBook })(BooksList);
+export default connect(mapStateToProps, { removeBook, changeFilter })(BooksList);
